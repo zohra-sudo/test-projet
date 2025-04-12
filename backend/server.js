9a788cc8709c94c117e9f9df88b05e63bef46f62
zohra El Ghomari <elghomarizohra@gmail.com>
2025-04-12 18:38:49 +0100

    ajout de workflow

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Backend temporaire pour l equipe et c est un petite test !!!'));
+// app.get('/', (req, res) => res.send('Backend temporaire pour l equipe et c est un petite test !!!'));
 app.listen(3000, () => console.log('Backend sur port 3000'));
